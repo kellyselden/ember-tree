981eedb5c295eb541f20e3b21e317196a2f91091
Kelly Selden <kellyselden@gmail.com>
2016-03-20 09:24:32 -0400

    moving tests

--- tests/integration/components/tree-node/is-selected-test.js
+++ tests/integration/components/tree-node/is-selected-test.js
@@ -1,8 +1,79 @@
+import Ember from 'ember';
 import { test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import init from './tree-node';
+import { init, renderDefault } from './tree-node';
+
+const {
+  set
+} = Ember;
 
 init();
+
+test('unchecked by default', function(assert) {
+  assert.expect(1);
+
+  renderDefault.call(this);
+
+  assert.notOk(this.$('input').prop('checked'));
+});
+
+test('can select checkbox', function(assert) {
+  assert.expect(1);
+
+  renderDefault.call(this);
+
+  this.$('input').click();
+
+  assert.ok(this.$('input').prop('checked'));
+});
+
+test('can unselect checkbox', function(assert) {
+  assert.expect(1);
+
+  renderDefault.call(this);
+
+  this.$('input').click();
+  this.$('input').click();
+
+  assert.notOk(this.$('input').prop('checked'));
+});
+
+test('isSelected is true on first click', function(assert) {
+  assert.expect(1);
+
+  let done = assert.async();
+
+  this.on('selectionChanged', isSelected => {
+    assert.ok(isSelected);
+
+    done();
+  });
+
+  renderDefault.call(this);
+
+  this.$('input').click();
+});
+
+test('isSelected is false on second click', function(assert) {
+  assert.expect(1);
+
+  let done = assert.async();
+
+  this.on('selectionChanged', (isSelected, model) => {
+    if (!isSelected) {
+      assert.ok(true);
+
+      done();
+    } else {
+      set(model, 'isSelected', isSelected);
+    }
+  });
+
+  renderDefault.call(this);
+
+  this.$('input').click();
+  this.$('input').click();
+});
 
 test('can start selected', function(assert) {
   // assert.expect(2);
--- tests/integration/components/tree-node/tree-node-test.js
+++ tests/integration/components/tree-node/tree-node-test.js
@@ -20,7 +20,7 @@ test('shows text', function(assert) {
 test('shows default loading text', function(assert) {
   assert.expect(1);
 
-  this.set('loadingText', undefined);
+  set(this, 'loadingText', undefined);
 
   renderDefault.call(this, false);
 
@@ -39,32 +39,12 @@ test('shows custom loading text', function(assert) {
   assert.strictEqual(this.$('.loading').text().trim(), 'test-loading');
 });
 
-test('has closed class by default', function(assert) {
-  assert.expect(2);
-
-  renderDefault.call(this);
-
-  assert.strictEqual(this.$('.closed').length, 1);
-  assert.strictEqual(this.$('.open').length, 0);
-});
-
-test('can be started open', function(assert) {
-  assert.expect(2);
-
-  this.set('model.isOpen', true);
-
-  renderDefault.call(this);
-
-  assert.strictEqual(this.$('.closed').length, 0);
-  assert.strictEqual(this.$('.open').length, 1);
-});
-
 test('does\'t send action when started open', function(assert) {
   assert.expect(1);
 
   let done = assert.async();
 
-  this.set('model.isOpen', true);
+  set(this, 'model.isOpen', true);
 
   let calledCount = 0;
 
@@ -81,29 +61,6 @@ test('does\'t send action when started open', function(assert) {
   renderDefault.call(this);
 
   this.$('.toggle').click();
-});
-
-test('has open class when clicked once', function(assert) {
-  assert.expect(2);
-
-  renderDefault.call(this);
-
-  this.$('.toggle').click();
-
-  assert.strictEqual(this.$('.closed').length, 0);
-  assert.strictEqual(this.$('.open').length, 1);
-});
-
-test('has closed class when clicked twice', function(assert) {
-  assert.expect(2);
-
-  renderDefault.call(this);
-
-  this.$('.toggle').click();
-  this.$('.toggle').click();
-
-  assert.strictEqual(this.$('.closed').length, 1);
-  assert.strictEqual(this.$('.open').length, 0);
 });
 
 test('has no content by default', function(assert) {
@@ -133,72 +90,6 @@ test('has no content when clicked twice', function(assert) {
   this.$('.toggle').click();
 
   assert.strictEqual(this.$('.children').text().trim(), '');
-});
-
-test('unchecked by default', function(assert) {
-  assert.expect(1);
-
-  renderDefault.call(this);
-
-  assert.notOk(this.$('input').prop('checked'));
-});
-
-test('can select checkbox', function(assert) {
-  assert.expect(1);
-
-  renderDefault.call(this);
-
-  this.$('input').click();
-
-  assert.ok(this.$('input').prop('checked'));
-});
-
-test('can unselect checkbox', function(assert) {
-  assert.expect(1);
-
-  renderDefault.call(this);
-
-  this.$('input').click();
-  this.$('input').click();
-
-  assert.notOk(this.$('input').prop('checked'));
-});
-
-test('isOpen is true on first click', function(assert) {
-  assert.expect(1);
-
-  let done = assert.async();
-
-  this.on('toggleChanged', isOpen => {
-    assert.ok(isOpen);
-
-    done();
-  });
-
-  renderDefault.call(this);
-
-  this.$('.toggle').click();
-});
-
-test('isOpen is false on second click', function(assert) {
-  assert.expect(1);
-
-  let done = assert.async();
-
-  this.on('toggleChanged', (isOpen, model) => {
-    if (!isOpen) {
-      assert.ok(true);
-
-      done();
-    } else {
-      set(model, 'isOpen', isOpen);
-    }
-  });
-
-  renderDefault.call(this);
-
-  this.$('.toggle').click();
-  this.$('.toggle').click();
 });
 
 test('sends model on toggle changed', function(assert) {
@@ -248,43 +139,6 @@ test('sends model on close', function(assert) {
 
   this.$('.toggle').click();
   this.$('.toggle').click();
-});
-
-test('isSelected is true on first click', function(assert) {
-  assert.expect(1);
-
-  let done = assert.async();
-
-  this.on('selectionChanged', isSelected => {
-    assert.ok(isSelected);
-
-    done();
-  });
-
-  renderDefault.call(this);
-
-  this.$('input').click();
-});
-
-test('isSelected is false on second click', function(assert) {
-  assert.expect(1);
-
-  let done = assert.async();
-
-  this.on('selectionChanged', (isSelected, model) => {
-    if (!isSelected) {
-      assert.ok(true);
-
-      done();
-    } else {
-      set(model, 'isSelected', isSelected);
-    }
-  });
-
-  renderDefault.call(this);
-
-  this.$('input').click();
-  this.$('input').click();
 });
 
 test('sends model on selection changed', function(assert) {
@@ -347,7 +201,7 @@ test('has toggle when branch node', function(assert) {
 test('can hide toggle', function(assert) {
   assert.expect(1);
 
-  this.set('model.shouldShowToggle', false);
+  set(this, 'model.shouldShowToggle', false);
 
   renderDefault.call(this);
 
